94fbb4f3130aeca346f092cffc8a9f16092288e4
PARK SUNG DONG <pasudo123@naver.com>
2019-08-16 16:20:40 +0900

    [refactor] 더 이상 개발 X, 인프라 쪽으로 집중하기.

---
+++ src/main/java/edu/pasudo123/board/core/config/EnvironmentConfiguration.java
@@ -0,0 +1,16 @@
+package edu.pasudo123.board.core.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by pasudo123 on 2019-08-15
+ * Blog: https://pasudo123.tistory.com/
+ * Email: oraedoa@gmail.com
+ **/
+@Configuration
+@EnableConfigurationProperties
+//@ConfigurationProperties(prefix = )
+public class EnvironmentConfiguration {
+}
